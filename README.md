de2ed66aa17677e2d035baf044d80a1ffe8e3227
Tannyzz <alejandro.plancarte@estudiante.uacm.edu.mx>
2015-07-23 18:35:05 -0500

    CDSC v.1.1

---
+++ README.md
@@ -0,0 +1,1 @@
+Website para Consultoría Dinámica en Sistemas de Calidad, v.1.1
---
+++ js/naveli-js.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+      $('.slider').slider({full_width: true});
+       $('.modal-trigger').leanModal();
+       //$('#modal1').openModal();
+       $(".button-collapse").sideNav();
+       $('.parallax').parallax();
+       $('.materialboxed').materialbox();
+    });
